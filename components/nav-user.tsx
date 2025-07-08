a33f473a78655ca1d0ad89a68918ca4f3e9ac33f
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-07-08 18:55:40 +0300

    feat: implement new admin page layout with sidebar and data integration; remove unused NavDocuments component

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
+
+import Link from "next/link";
 
 import {
-  IconCreditCard,
+  IconDashboard,
   IconDotsVertical,
+  IconHome,
   IconLogout,
-  IconNotification,
-  IconUserCircle,
-} from "@tabler/icons-react"
+} from "@tabler/icons-react";
+import { Tv2 } from "lucide-react";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -21,24 +19,22 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
+export function NavUser() {
+  const { isMobile } = useSidebar();
+  const { data: session, isPending } = authClient.useSession();
+
+  if (isPending) {
+    return null;
   }
-}) {
-  const { isMobile } = useSidebar()
 
   return (
     <SidebarMenu>
@@ -49,14 +45,28 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage
+                  src={
+                    session?.user.image ??
+                    `https://avatar.vercel.sh/${session?.user.email}`
+                  }
+                  alt={session?.user.name}
+                />
+                <AvatarFallback className="rounded-lg">
+                  {session?.user.name && session?.user.name.length > 0
+                    ? session.user.name.charAt(0).toUpperCase()
+                    : session?.user.email.charAt(0).toUpperCase()}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate font-medium">
+                  {session?.user.name && session?.user.name.length > 0
+                    ? session.user.name
+                    : session?.user.email.split("@")[0]}
+                </span>
                 <span className="text-muted-foreground truncate text-xs">
-                  {user.email}
+                  {session?.user.email}
                 </span>
               </div>
               <IconDotsVertical className="ml-auto size-4" />
@@ -71,30 +81,44 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarImage
+                    src={
+                      session?.user.image ??
+                      `https://avatar.vercel.sh/${session?.user.email}`
+                    }
+                    alt={session?.user.name}
+                  />
+                  <AvatarFallback className="rounded-lg">
+                    {session?.user.name && session?.user.name.length > 0
+                      ? session.user.name.charAt(0).toUpperCase()
+                      : session?.user.email.charAt(0).toUpperCase()}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">
+                    {session?.user.name && session?.user.name.length > 0
+                      ? session.user.name
+                      : session?.user.email.split("@")[0]}
+                  </span>
                   <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
+                    {session?.user.email}
                   </span>
                 </div>
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <IconUserCircle />
-                Account
+              <DropdownMenuItem asChild>
+                <IconHome />
+                <Link href={"/"}>Home</Link>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconCreditCard />
-                Billing
+              <DropdownMenuItem asChild>
+                <IconDashboard />
+                <Link href={"/admin"}>Dashboard</Link>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconNotification />
-                Notifications
+              <DropdownMenuItem asChild>
+                <Tv2 />
+                <Link href={"/admin/courses"}>Notifications</Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
@@ -106,5 +130,5 @@ export function NavUser({
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
-  )
+  );
 }
